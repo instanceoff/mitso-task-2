b0f2f70d65e1c312b6d71750b25480ae3768669b
Ilya Khinevich <ilyaxinevich@gmail.com>
2021-12-15 14:55:01 +0300

    little debag

--- src/resources/menus/menu.model.ts
+++ src/resources/menus/menu.model.ts
@@ -1,8 +1,12 @@
 export class Menu {
   id: string;
+
   title: string;
+
   photo: string;
+
   isPublish: boolean;
+
   constructor({ id = 'id', title = 'title', photo = 'photo', isPublish = true } = {}) {
     this.id = id;
     this.title = title;
